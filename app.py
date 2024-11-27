cdc6c29bb1f695906471c0b459165cff9d411e38
Vatsal Gupta <97608916+vatsalgupta25@users.noreply.github.com>
2024-11-27 18:51:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-    return '<h1>Let's see if this works.</h1>'
+    return '<h1>Let\'s see if this works.</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
